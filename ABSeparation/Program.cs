abcc86a4c4fa87d5f1ff608e3530aaaea5b86138
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:35:57 -0500

    message

---
+++ ABSeparation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABSeparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteBanner();
+
+            Console.WriteLine("Enter a String of characters with A and B in it:");
+            string userInput = Console.ReadLine();
+            char[] arr;
+            arr = userInput.ToCharArray();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                //test only left numbers only if index is at least 3
+                //test only right as long as index is at least max length - 3
+                if(i < (arr.Length - 4) )
+                {
+                    if(arr[i+4] == 'a' && arr[i] == 'b' || arr[i+4] == 'b' && arr[i] == 'a')
+                    {
+                        Console.WriteLine("String contains a and b separated by 3 digits!");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+
+            }
+            Console.WriteLine("String does not contain a and b separated by 3 digits!");
+            Console.ReadKey();
+            return;
+        }
+
+        static void WriteBanner()
+        {
+            Console.WriteLine(@"+--------------------------------------------------------+");
+            Console.WriteLine(@"|      __         _____        __    _____               |");
+            Console.WriteLine(@"|  ___/ /__ _  __/ ___/__  ___/ /__ / ___/__ ___ _  ___  |");
+            Console.WriteLine(@"| / _  / -_) |/ / /__/ _ \/ _  / -_) /__/ _ `/  ' \/ _ \ |");
+            Console.WriteLine(@"| \_,_/\__/|___/\___/\___/\_,_/\__/\___/\_,_/_/_/_/ .__/ |");
+            Console.WriteLine(@"|                                                /_/     |");
+            Console.WriteLine(@"+--------------------------------------------------------+");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ABSeparation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ABSeparation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ABSeparation")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2f90e81-f206-4a41-97de-60fa42898454")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
